d6fa6d1c2ac9e3b0e6478230c8ff3f50d6ec5ef9
Avinash Kr Mandal <avinashkrmandal03@gmail.com>
2025-12-19 23:10:07 +0530

    server setup

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,12 @@
-import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import cors from "cors";
-
-const app = express();
-
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    withCredentials: true,
-  })
-);
-
-app.use(express.json());
+import app from "./src/app.js";
+import connectToDb from "./src/config/db.js";
 
 const PORT = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send("CampusBot is Ready")
-})
-
-app.listen(PORT, () => {
-  console.log(`CampusBot is Running on ${PORT}`);
+connectToDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`CampusBot is Running on ${PORT}`);
+  });
 });
---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("CampusBot is Ready");
+});
+
+export default app;
